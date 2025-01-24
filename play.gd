931372c8fa021f7a2408c2cd155bf8602dd862cf
Allppatt <alfathrausyann@gmail.com>
2025-01-25 00:30:36 +0700

    commit ke 3

--- play.gd
+++ play.gd
@@ -6,7 +6,7 @@ func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
-func _on_option_pressed() -> void:
+func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://options_menu.tscn")
 
 
